8ede8c503bd7d7d9f259a54b5c97151888cf15eb
Sibbe Heijne <sibbe.webmaster@gmail.com>
2017-01-10 18:06:23 +0100

    Removed annoying stuff ...

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 ## install dotfiles, assumes vim exists
 
 # install necessary applications
-apt-get install tmux 
-apt-get install silversearcher-ag
-apt-get install git
+#apt-get install tmux 
+#apt-get install silversearcher-ag
+#apt-get install git
 
 mkdir -p ~/.vim
 mkdir -p ~/.vim/plugins
 mkdir -p ~/.vim/colors
 
 # move all dot files to home directory
-mv .tmux.conf ~
-mv .bashrc ~
+#mv .tmux.conf ~
+#mv .bashrc ~
 mv .vimrc ~
 
 # vim settings
